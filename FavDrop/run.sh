27349538f2a580c8251f61d59c78a65a8042a66f
Ubuntu <loca@LocaAppHost.LocaAppHost.m7.internal.cloudapp.net>
2016-10-31 11:37:55 +0000

    stop and remove FavDrop container in run.sh

--- FavDrop/run.sh
+++ FavDrop/run.sh
@@ -1,3 +1,16 @@
 #/bin/sh
 
+# remove running fav-drop container if exists
+fav_drop_container=`docker ps -a --filter='status=running' | grep "fav-drop" | awk '{print $1}'`
+if [ ! -z "${fav_drop_container}" ] ; then
+	echo "running fav-drop container: $fav_drop_container"
+
+	echo "stop fav_drop_container"
+	docker stop $fav_drop_container
+
+	echo "remove fav_drop_container"
+	docker rm $fav_drop_container
+fi
+
+echo "run fav-drop container"
 docker run --name fav-drop -d loca/fav-drop mono "FavDrop/FavDrop.exe"
